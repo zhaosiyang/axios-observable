55ce5127d2ff20af527ac38b331938802726aaf1
Hannes Widrig <hanneswidrig@users.noreply.github.com>
2019-02-07 19:05:59 -0500

    Update index.ts

--- lib/index.ts
+++ lib/index.ts
@@ -1,83 +1,121 @@
-import {AxiosInstance, AxiosInterceptorManager, AxiosRequestConfig, AxiosResponse} from "axios";
-import axios from 'axios';
-import {AxiosObservable} from "./axios-observable.interface";
-import {promiseToObservable} from "./promise-to-observable";
+import axios, {
+  AxiosInstance,
+  AxiosInterceptorManager,
+  AxiosRequestConfig,
+  AxiosResponse,
+} from 'axios'
+import { IAxiosObservable } from './axios-observable.interface'
+import { promiseToObservable } from './promise-to-observable'
 
 class Axios {
-
-  constructor(private axiosInstance: AxiosInstance) {
-  }
-
-  static defaults: AxiosRequestConfig = axios.defaults;
+  static defaults: AxiosRequestConfig = axios.defaults
   static interceptors: {
-    request: AxiosInterceptorManager<AxiosRequestConfig>;
-    response: AxiosInterceptorManager<AxiosResponse>;
-  } = axios.interceptors;
+    request: AxiosInterceptorManager<AxiosRequestConfig>
+    response: AxiosInterceptorManager<AxiosResponse>
+  } = axios.interceptors
 
-  static request<T=any>(config: AxiosRequestConfig): AxiosObservable<T> {
-    return promiseToObservable(axios.request(config));
+  static request<T = any>(config: AxiosRequestConfig): IAxiosObservable<T> {
+    return promiseToObservable(axios.request(config))
   }
 
-  static get<T=any>(url: string, config?: AxiosRequestConfig): AxiosObservable<T> {
-    return promiseToObservable(axios.get(url, config));
+  static get<T = any>(
+    url: string,
+    config?: AxiosRequestConfig
+  ): IAxiosObservable<T> {
+    return promiseToObservable(axios.get(url, config))
   }
 
-  static post<T=any>(url: string, data?: any, config?: AxiosRequestConfig): AxiosObservable<T> {
-    return promiseToObservable(axios.post(url, data, config));
+  static post<T = any>(
+    url: string,
+    data?: any,
+    config?: AxiosRequestConfig
+  ): IAxiosObservable<T> {
+    return promiseToObservable(axios.post(url, data, config))
   }
 
-  static put<T=any>(url: string, data?: any, config?: AxiosRequestConfig): AxiosObservable<T> {
-    return promiseToObservable(axios.put(url, data, config));
+  static put<T = any>(
+    url: string,
+    data?: any,
+    config?: AxiosRequestConfig
+  ): IAxiosObservable<T> {
+    return promiseToObservable(axios.put(url, data, config))
   }
 
-  static patch<T=any>(url: string, data?: any, config?: AxiosRequestConfig): AxiosObservable<T> {
-    return promiseToObservable(axios.patch(url, data, config));
+  static patch<T = any>(
+    url: string,
+    data?: any,
+    config?: AxiosRequestConfig
+  ): IAxiosObservable<T> {
+    return promiseToObservable(axios.patch(url, data, config))
   }
 
-  static delete<T=any>(url: string, config?: AxiosRequestConfig): AxiosObservable<T> {
-    return promiseToObservable(axios.delete(url, config));
+  static delete<T = any>(
+    url: string,
+    config?: AxiosRequestConfig
+  ): IAxiosObservable<T> {
+    return promiseToObservable(axios.delete(url, config))
   }
 
-  static head<T=any>(url: string, config?: AxiosRequestConfig): AxiosObservable<T> {
-    return promiseToObservable(axios.head(url, config));
+  static head<T = any>(
+    url: string,
+    config?: AxiosRequestConfig
+  ): IAxiosObservable<T> {
+    return promiseToObservable(axios.head(url, config))
   }
 
   static create(config: AxiosRequestConfig): Axios {
-    return new Axios(axios.create(config));
+    return new Axios(axios.create(config))
   }
 
-  request<T=any>(config: AxiosRequestConfig): AxiosObservable<T> {
-    return promiseToObservable(this.axiosInstance.request(config));
+  constructor(private axiosInstance: AxiosInstance) {}
+
+  request<T = any>(config: AxiosRequestConfig): IAxiosObservable<T> {
+    return promiseToObservable(this.axiosInstance.request(config))
   }
 
-  get<T=any>(url: string, config?: AxiosRequestConfig): AxiosObservable<T> {
-    return promiseToObservable(this.axiosInstance.get(url, config));
+  get<T = any>(url: string, config?: AxiosRequestConfig): IAxiosObservable<T> {
+    return promiseToObservable(this.axiosInstance.get(url, config))
   }
 
-  head<T=any>(url: string, config?: AxiosRequestConfig): AxiosObservable<T> {
-    return promiseToObservable(this.axiosInstance.head(url, config));
+  head<T = any>(url: string, config?: AxiosRequestConfig): IAxiosObservable<T> {
+    return promiseToObservable(this.axiosInstance.head(url, config))
   }
 
-  post<T=any>(url: string, data?: any, config?: AxiosRequestConfig): AxiosObservable<T> {
-    return promiseToObservable(this.axiosInstance.post(url, data, config));
+  post<T = any>(
+    url: string,
+    data?: any,
+    config?: AxiosRequestConfig
+  ): IAxiosObservable<T> {
+    return promiseToObservable(this.axiosInstance.post(url, data, config))
   }
 
-  put<T=any>(url: string, data?: any, config?: AxiosRequestConfig): AxiosObservable<T> {
-    return promiseToObservable(this.axiosInstance.put(url, data, config));
+  put<T = any>(
+    url: string,
+    data?: any,
+    config?: AxiosRequestConfig
+  ): IAxiosObservable<T> {
+    return promiseToObservable(this.axiosInstance.put(url, data, config))
   }
 
-  patch<T=any>(url: string, data?: any, config?: AxiosRequestConfig): AxiosObservable<T> {
-    return promiseToObservable(this.axiosInstance.patch(url, data, config));
+  patch<T = any>(
+    url: string,
+    data?: any,
+    config?: AxiosRequestConfig
+  ): IAxiosObservable<T> {
+    return promiseToObservable(this.axiosInstance.patch(url, data, config))
   }
 
-  delete<T=any>(url: string, config?: AxiosRequestConfig): AxiosObservable<T> {
-    return promiseToObservable(this.axiosInstance.delete(url, config));
+  delete<T = any>(
+    url: string,
+    config?: AxiosRequestConfig
+  ): IAxiosObservable<T> {
+    return promiseToObservable(this.axiosInstance.delete(url, config))
   }
 
   get defaults() {
-    return this.axiosInstance.defaults;
+    return this.axiosInstance.defaults
   }
 }
 
-export default Axios;
-export {Axios};
+export default Axios
+export { Axios }
