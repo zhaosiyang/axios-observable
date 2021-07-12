ae53478e5206202a7e76cd25bb57ca5e6a03fa4f
liusonghui <itessay@163.com>
2021-07-13 01:32:47 +0800

    fix: Property 'subscribe' does not exist on type 'AxiosObservable'

--- lib/axios-observable.interface.ts
+++ lib/axios-observable.interface.ts
@@ -1,5 +1,4 @@
 import {AxiosResponse} from 'axios';
 import {Observable} from 'rxjs';
 
-export interface AxiosObservable<T> extends Observable<AxiosResponse<T>> {
-}
+export type  AxiosObservable<T> = Observable<AxiosResponse<T>>
