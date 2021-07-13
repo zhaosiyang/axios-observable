285a9622285e89b08d389e679eb3811956c44dc1
Kern Zhao <siyangkernzhao@gmail.com>
2021-07-13 01:48:36 -0400

    Update target to es2015 in tsconfig

--- lib/axios-observable.interface.ts
+++ lib/axios-observable.interface.ts
@@ -1,4 +1,4 @@
 import {AxiosResponse} from 'axios';
 import {Observable} from 'rxjs';
 
-export type  AxiosObservable<T> = Observable<AxiosResponse<T>>
+export type AxiosObservable<T> = Observable<AxiosResponse<T>>
